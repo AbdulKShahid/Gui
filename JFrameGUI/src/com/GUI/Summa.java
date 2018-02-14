de2cb19ff0cf8b8d58018c7d4a3847a42a62a3df
AbdulKShahid <abdulkshahid@gmail.com>
2018-02-14 01:13:22 +0100

    Add files via upload

---
+++ JFrameGUI/src/com/GUI/Summa.java
@@ -0,0 +1,9 @@
+package com.GUI;
+
+public class Summa {
+
+	public static String uname="name";
+	public String password = "password";
+	public static String emailId = "ab@gmail.com";
+	public String userid= "123";
+}
